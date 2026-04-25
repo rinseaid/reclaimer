cdf19b343656a78f67ec723aa11b11c3bb428b47
rinseaid <rinseaid@rinseaid.net>
2026-04-25 12:31:24 -0400

    Always show local login if local accounts exist in DB

--- internal/viewer/viewer.go
+++ internal/viewer/viewer.go
@@ -44,6 +44,12 @@ func (s *Server) LeavingRoutes() chi.Router {
 	return r
 }
 
+func (s *Server) hasLocalUsers() bool {
+	var count int
+	s.DB.Get(&count, "SELECT COUNT(*) FROM viewer_users WHERE auth_provider = 'local'")
+	return count > 0
+}
+
 func (s *Server) hasAnyAdmins() bool {
 	var count int
 	s.DB.Get(&count, "SELECT COUNT(*) FROM viewer_users WHERE is_admin = 1")
@@ -74,7 +80,8 @@ func (s *Server) handleAuthStatus(w http.ResponseWriter, r *http.Request) {
 			"display_name": s.Config.GetString("viewer_oidc_display_name"),
 		},
 		"local": map[string]any{
-			"enabled": s.Config.GetBool("viewer_local_enabled"),
+			"enabled":   s.Config.GetBool("viewer_local_enabled"),
+			"has_users": s.hasLocalUsers(),
 		},
 	})
 }
